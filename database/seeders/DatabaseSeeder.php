85d8e96b85a4b231bbc8e8a1ac5c23d760f35629
vodinhht20 <dinhvv@topcv.vn>
2022-04-26 16:04:42 +0700

    un command seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,10 +21,10 @@ class DatabaseSeeder extends Seeder
         // \App\Models\Category::factory(10)->create();
         // \App\Models\User::factory(10)->create();
 
-        // $role = Role::create(['name' => 'admin']);
-        // $permission = Permission::create(['name' => 'cpanel']);
-        // $role->givePermissionTo($permission);
-        // $user = User::find(1);
-        // $user->assignRole('admin');
+        $role = Role::create(['name' => 'admin']);
+        $permission = Permission::create(['name' => 'cpanel']);
+        $role->givePermissionTo($permission);
+        $user = User::find(1);
+        $user->assignRole('admin');
     }
 }
