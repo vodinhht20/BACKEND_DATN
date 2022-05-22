2d3465de8d1cdce5a281ef4a66cbfa4e214a064f
vodinhht20 <vodinh2000ht@gmail.com>
2022-05-22 23:22:04 +0700

    fix ui timesheet

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -3,32 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Carbon\Carbon;
-use DateTimeZone;
 class CompanyController extends Controller
 {
     public function info(Request $request)
     {
         return view('admin.company.info');
-    }
-    public function worker(Request $request)
-    {
-        
-        $carbon= Carbon::now();
-        $carbon= Carbon::now(new DateTimeZone('Asia/Ho_Chi_Minh'));
-        $carbon = Carbon::now()->daysInMonth;
-        $month = Carbon::now()->monthName;
-        $month = Carbon::now()->format('n ');
-        $date = Carbon::now($month)->format('n');                                           
-        $day = Carbon::now($date)->dayName;
-        $day = Carbon::now($date)->format('n'); ;
-        $date = Carbon::now($month)->format('d / m'); 
-        $bef_month = Carbon::now()->startOfMonth()->subMonth($month);
-
-        return view('admin.company.worker',compact('month','bef_month','day','date'));
-        
-                                    
-
     }
 }
 ?>
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
 use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\TimesheetController;
 
 /*
 |--------------------------------------------------------------------------
@@ -96,7 +97,6 @@ Route::get('google/callback', [AuthController::class, 'ggAuthCallback'])->name('
 
 Route::prefix('/company')->name("company.")->group(function() {
     Route::get('/info', [CompanyController::class, 'info'])->name("info");
-    Route::get('/worker', [CompanyController::class, 'worker'])->name("worker");
-
 });
+Route::get('/timesheet', [TimesheetController::class, 'timesheet'])->name("timesheet");
 
