0d2f9e45e843f1751416aee5b4f6a0bf29d38a7f
vodinhht20 <vodinh2000ht@gmail.com>
2022-05-23 22:51:06 +0700

    fix authenticate api, add cors policy

--- config/auth.php
+++ config/auth.php
@@ -14,7 +14,7 @@ return [
     */
 
     'defaults' => [
-        'guard' => 'api',
+        'guard' => 'web',
         'passwords' => 'users',
     ],
 
@@ -41,9 +41,9 @@ return [
             'provider' => 'users',
         ],
         'api' => [
-            'driver' => 'session', //jwt
+            'driver' => 'jwt',
             'provider' => 'users',
-        ],
+        ]
     ],
 
     /*
