77190af8d18cb211e8649089fd548216b5322f6f
van dat <datnvph13011@fpt.edu.vn>
2022-05-25 08:27:54 +0700

    update company

--- resources/views/admin/company/info.blade.php
+++ resources/views/admin/company/info.blade.php
@@ -47,6 +47,12 @@
                     Hệ thống <br> chi nhánh</a>
                 <div class="slide"></div>
             </li>
+            <li class="nav-item">
+                <a class="nav-link" data-toggle="tab" href="#" role="tab" aria-expanded="false">
+                    <i class="ti-wallet"></i><br>
+                    Cơ cấu <br> tổ chức</a>
+                <div class="slide"></div>
+            </li>
         </ul>
         <!-- Tab panes -->
         <div class="tab-content tabs-left-content card-block">
