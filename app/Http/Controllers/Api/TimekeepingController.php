1360e6151b5d8e3abd908fad421c4e62b6484002
trantien <trantien160398@gmail.com>
2022-06-01 16:08:32 +0700

    update check ip

--- app/Http/Controllers/Api/TimekeepingController.php
+++ app/Http/Controllers/Api/TimekeepingController.php
@@ -8,19 +8,22 @@ use Illuminate\Http\Request;
 class TimekeepingController extends Controller
 {
     public function checkIn(Request $request){
-        $ipCompany = '14.232.245.144';
-        if ($request->ip() == $ipCompany) {
-            return response()->json([
-                'message' => 'checkin thành công',
-                'ip' => $request->ip(),
-                'error_code' => 80
-            ]);
-        }
 
+        $ipCompany = ['14.232.245.144', '127.0.0.1'];
+        foreach ($ipCompany as $ip) {
+            if ($request->ip() == $ip) {
+                return response()->json([
+                    'message' => 'checkin thành công',
+                    'ip' => $request->ip(),
+                    'error_code' => 80
+                ]);
+            }
+        }
         return response()->json([
             'message' => 'checkin thất bại vui lòng kết nối Wifi công ty để điểm danh',
             'ip' => $request->ip(),
             'error_code' => 73
         ]);
+        
     }
 }
