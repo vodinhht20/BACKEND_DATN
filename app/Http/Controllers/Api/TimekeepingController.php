6183870bfd8ecfbebb1d204f9533fe409516a412
trantien <trantien160398@gmail.com>
2022-06-06 11:14:56 +0700

    update check mac

--- app/Http/Controllers/Api/TimekeepingController.php
+++ app/Http/Controllers/Api/TimekeepingController.php
@@ -9,20 +9,28 @@ class TimekeepingController extends Controller
 {
     public function checkIn(Request $request){
 
-        $ipCompany = ['14.232.245.144', '127.0.0.1'];
-        foreach ($ipCompany as $ip) {
-            if ($request->ip() == $ip) {
-                return response()->json([
-                    'message' => 'checkin thành công',
-                    'ip' => $request->ip(),
-                    'error_code' => 80
-                ]);
-            }
+        $ipAndMac = ['ip' => '14.232.245.144', 'mac' => '00-FF-A1-90-36-28'];
+
+        ob_start();
+        system('getmac');
+        $Content = ob_get_contents();
+        ob_clean();
+        $mac = substr($Content, strpos($Content,'\\')-20, 17);
+
+        if ($request->ip() == $ipAndMac['ip'] && $ipAndMac['mac'] == $mac) {
+            return response()->json([
+                'message' => 'checkin thành công',
+                'ip' => $request->ip(),
+                'error_code' => 80,
+                'mac' => $mac,
+            ]);
         }
+
         return response()->json([
             'message' => 'checkin thất bại vui lòng kết nối Wifi công ty để điểm danh',
             'ip' => $request->ip(),
-            'error_code' => 73
+            'error_code' => 73,
+            'mac' => $mac,
         ]);
         
     }
