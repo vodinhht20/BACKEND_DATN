5315bd1a36201b19a87b5679a67722d2c23b436e
trantien <trantien160398@gmail.com>
2022-06-07 09:38:46 +0700

    update CORS

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -63,7 +63,9 @@ class AuthController extends Controller
     {
         return response()->json([
             'access_token' => $token
-        ], 200);
+        ], 200)->header('Access-Control-Allow-Origin', '*')
+        ->header('Access-Control-Allow-Methods','*')
+        ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     }
 
     public function googleLogin(Request $request): JsonResponse
