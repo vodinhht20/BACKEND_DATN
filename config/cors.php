3c8693ecfac9ae8e5ff3aa8a240309e7d1767fd6
trantien <trantien160398@gmail.com>
2022-06-14 17:26:34 +0700

    fix login gg

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'oauth/*' , 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'api'],
 
     'allowed_methods' => ['*'],
 
