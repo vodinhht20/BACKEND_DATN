433c29489b92558509f59ec767e73694bb847d87
van dat <datnvph13011@fpt.edu.vn>
2022-06-16 15:12:59 +0700

    update css company

--- resources/views/admin/company/structure.blade.php
+++ resources/views/admin/company/structure.blade.php
@@ -38,18 +38,18 @@
                     <i class="ti-layout-media-overlay"></i><br>
                     Thông tin <br> công ty</a>
             </li>
-            <li class="nav-item">
+            <li class="nav-item btn-item">
                 <a class="nav-link" href="{{ route('company.branchs') }}" role="tab" aria-expanded="false">
                     <i class="ti-wallet"></i><br>
                     Hệ thống <br> chi nhánh</a>
             </li>
-            <li class="nav-item active">
+            <li class="nav-item btn-item active">
                 <a class="nav-link" href="{{ route('company.structure') }}" role="tab" aria-expanded="false">
                     <i class="ti-wallet"></i><br>
                     Cơ cấu <br> tổ chức</a>
             </li>
         </ul>
-        <div class="treeview js-treeview">
+        <div style="overflow-x:auto;" class="treeview js-treeview">
             <ul>
                 <li>
                     <div class="treeview__level" data-level="A">
