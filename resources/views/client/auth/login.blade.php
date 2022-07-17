1c984c21938e7bc2453f28fe27e01f5e582024f8
vodinhht20 <vodinh2000ht@gmail.com>
2022-07-17 11:03:06 +0700

    add google recapcha

--- resources/views/client/auth/login.blade.php
+++ resources/views/client/auth/login.blade.php
@@ -6,87 +6,70 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Đăng nhập Camel</title>
-    <link rel="stylesheet" type="text/css" href="{{ asset('frontend') }}/css/login.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('frontend') }}/css/login.css?v=v1.0.1">
+    <script src='https://www.google.com/recaptcha/api.js'></script>
 </head>
-
 <body>
-
-    <body id="particles-js"></body>
-    <div class="animated bounceInDown">
-        <div class="container">
-            <span class="error animated tada" id="msg"></span>
-            <div id="notify" class="mt-2">
-                @if (Session::has('message.error'))
-                    <p class="error2 text-validate">{{ Session::get('message.error') }}</p>
-                @endif
-                @if (Session::has('message.success'))
-                    <p class="error2 text-success">{{ Session::get('message.success') }}</p>
-                @endif
-            </div>
-            <form name="form1" method="POST" class="box" onsubmit="return checkStuff()">
+    <div class="login-page container">
+        <div class="wrapper-content">
+            <form id="form_login" method="POST" class="box">
                 @csrf
-                <h4>Camel<span> Dashboard</span></h4>
-                <h5>Đăng nhập vào tài khoản của bạn.</h5>
-                <input type="text" name="email" value="{{ old('email') }}" placeholder="Email" autocomplete="off">
-                <i class="typcn typcn-eye" id="eye"></i>
-                <input type="password" name="password" placeholder="Passsword" id="pwd" autocomplete="off">
-                <a href="#" class="forgetpass">Quên mật khẩu?</a>
-                <input type="submit" value="Đăng nhập" class="btn1">
+                <div class="header">
+                    <h4>Camel<span> Dashboard</span></h4>
+                    <h5>Đăng nhập vào tài khoản của bạn.</h5>
+                </div>
+                <div class="wrapper-input">
+                    <div class="form-group">
+                        <input type="text" name="email" value="{{ old('email') }}" placeholder="Email" autocomplete="off">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" name="password" placeholder="Passsword" id="pwd" autocomplete="off">
+                    </div>
+                    <div class="form-group">
+                        <div class="g-recaptcha" data-sitekey="{{ env('GOOGLE_RECAPTCHA_KEY') }}"></div>
+                    </div>
+                </div>
+                <div id="notify" class="mt-2">
+                    @if (Session::has('message.error'))
+                        <p class="text-validate">{{ Session::get('message.error') }}</p>
+                    @endif
+                    @if (Session::has('message.success'))
+                        <p class="text-success">{{ Session::get('message.success') }}</p>
+                    @endif
+                </div>
+                <div class="wrapper_btn">
+                    <input type="submit" value="Đăng nhập" class="btn-login">
+                </div>
             </form>
-            <div class="login-social-network mt-5 mb-5">
-                <a href="{{route('login-google')}}" class="login-google"><img src="{{asset('frontend')}}/image/login-google.png" alt=""></a>
-                <a href="{{route('login-github')}}" class="login-google"><img src="{{asset('frontend')}}/image/login-github.png" alt=""></a>
+            <div class="line-break mt-4">
+                <span>hoặc đăng nhập bằng</span>
             </div>
-            <a href="#" class="dnthave">Không có tài khoản? Đăng ký</a>
+            <div class="login-social-network">
+                <a href="{{route('login-google')}}" class="login-google">
+                    <img src="{{asset('frontend')}}/image/logo-google.png" alt="">
+                    <span>Đăng nhập với tài khoản Google</span>
+                </a>
+                {{-- <a href="{{route('login-github')}}" class="login-google"><img src="{{asset('frontend')}}/image/login-github.png" alt=""></a> --}}
+            </div>
+            <a href="#" class="forgot_password">Quên mật khẩu?</a>
         </div>
     </div>
-
-    <script>
-        var pwd = document.getElementById("pwd");
-        var eye = document.getElementById("eye");
-
-        eye.addEventListener("click", togglePass);
-
-        function togglePass() {
-            eye.classList.toggle("active");
-
-            pwd.type == "password" ? (pwd.type = "text") : (pwd.type = "password");
-        }
-
-        // Form Validation
-
-        function checkStuff() {
-            var email = document.form1.email;
-            var password = document.form1.password;
-            var msg = document.getElementById("msg");
-
-            if (email.value == "") {
-                msg.style.display = "block";
-                msg.innerHTML = "Vui lòng nhập email của bạn";
-                email.focus();
-                return false;
-            } else {
-                msg.innerHTML = "";
-            }
-
-            if (password.value == "") {
-                msg.innerHTML = "Vui lòng nhập mật khẩu của bạn";
-                password.focus();
-                return false;
-            } else {
-                msg.innerHTML = "";
-            }
-            var re =
-                /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-            if (!re.test(email.value)) {
-                msg.innerHTML = "Vui lòng nhập email hợp lệ";
-                email.focus();
-                return false;
-            } else {
-                msg.innerHTML = "";
-            }
-        }
-    </script>
 </body>
-
+<!-- validate -->
+<script type="text/javascript" src="{{asset('assets')}}/js/jquery/jquery.min.js"></script>
+<script type="text/javascript" src="{{asset('frontend')}}/js/jquery.validate.min.js"></script>
+<script type="text/javascript" src="{{asset('frontend')}}/js/validateForm.js"></script>
+<script>
+    const objData = {
+    rules: {
+        email: "required",
+        password: "required",
+    },
+    messages: {
+        email: `<span class="text-validate">Vui lòng nhập email</span>`,
+        password: `<span class="text-validate">Vui lòng nhập password</span>`
+    }
+  }
+  validateForm("#form_login", objData);
+</script>
 </html>
