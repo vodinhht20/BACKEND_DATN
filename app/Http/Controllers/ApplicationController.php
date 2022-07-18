341ac50ce6a6ceca5f224dac60d716efa4da03fd
vodinhht20 <dinhvv@topcv.vn>
2022-07-18 18:05:20 +0700

    update code

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -2,25 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Repositories\SingleTypeRepository;
 use Illuminate\Http\Request;
 
 class ApplicationController extends Controller
 {
-    public function index(){
+    public function __construct(private SingleTypeRepository $singleTypeRepo)
+    {
+        //
+    }
+
+    public function index()
+    {
         return view('admin.application.view');
     }
-    public function detail(){
+    public function detail()
+    {
         return view('admin.application.detail');
     }
-    public function nestView(){
-        return view('admin.application.viewnest');
+    public function nestView()
+    {
+        $options = [
+            'with' => 'approvers'
+        ];
+        $singleTypes = $this->singleTypeRepo->getPublicSingleType($options);
+        return view('admin.application.viewnest', compact('singleTypes'));
     }
-    public function policy(){
+    public function policy()
+    {
         return view('admin.application.policy');
     }
-    public function procedure(){
+    public function procedure()
+    {
         return view('admin.application.procedure');
     }
-    
+
 
 }
--- app/Models/SingleType.php
+++ app/Models/SingleType.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class SingleType extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
     protected $table = 'single_types';
 
     public function approvers()
