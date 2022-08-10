23c4b7efaf41f8572ee2dc90cb0e5f8dfad01878
trantienvip <trantienvip>
2022-08-10 22:52:03 +0700

    fix link notification

--- app/Models/Notifications.php
+++ app/Models/Notifications.php
@@ -15,13 +15,13 @@ class Notifications extends Model
 
     public function getLink()
     {
-        if ($this->link && trim($this->link) != '') {
-            if ($this->domain == config('notification.domain.BE')) {
-                return env('DOMAIN_BACKEND') . "/admin/$this->link";
-            } else if ($this->domain == config('notification.domain.FE')) {
-                return env('DOMAIN_FRONTEND') . $this->link;
-            }
-        }
+        // if ($this->link && trim($this->link) != '') {
+        //     if ($this->domain == config('notification.domain.BE')) {
+        //         return env('DOMAIN_BACKEND') . "/admin/$this->link";
+        //     } else if ($this->domain == config('notification.domain.FE')) {
+        //         return env('DOMAIN_FRONTEND') . $this->link;
+        //     }
+        // }
         return $this->link;
     }
 }
